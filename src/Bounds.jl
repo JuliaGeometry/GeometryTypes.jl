9c80daf7d7e9d8e7f22ed083834d3e4628c2dfea
Steve Kelly <kd2cca@gmail.com>
2015-02-07 19:25:48 -0500

    first pass at bounds types

--- src/Bounds.jl
+++ src/Bounds.jl
@@ -1,5 +1,27 @@
 module Bounds
 
-# package code goes here
+abstract AbstractBounds{T, N}
+
+type Bounds{T, N<:Int} <: AbstractBounds{T, N}
+    min::Vector{T}
+    max::Vector{T}
+end
+
+function Bounds{T}(max::Vector{T}, min::Vector{T})
+    n = length(max)
+    m = length(min)
+    @assert n == m
+    Bounds{T, n}(max, min)
+end
+
+max(b::Bounds) = b.max
+min(b::Bounds) = b.min
+
+function update!{T, N}(b::Bounds{T, N}, v)
+    for i = 1:n
+        b.max[i] = max(b.max[i], v[i])
+        b.min[i] = min(b.min[i], v[i])
+    end
+end
 
 end # module
