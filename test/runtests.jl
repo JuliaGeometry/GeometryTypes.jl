a07a7d01869d771719da7ca0073809c521b2f289
Steve Kelly <kd2cca@gmail.com>
2015-02-07 19:58:11 -0500

    add tests for `contains` and `in`

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,3 +14,10 @@ update!(a, [5,6,7,8])
 
 @test a == Bound{Float64,4}([1.0,2.0,3.0,4.0],[5.0,6.0,7.0,8.0])
 
+b = Bound{Float64,4}([1.0,2.0,3.0,4.0],[5.0,6.0,7.0,8.0])
+
+@test in(a,b) && in(b,a) && contains(a,b) && contains(b,a)
+
+c = Bound{Float64,4}([1.1,2.1,3.1,4.1],[4.0,5.0,6.0,7.0])
+
+@test !in(a,c) && in(c,a) && contains(a,c) && !contains(c,a)
