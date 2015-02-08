4e42f2f1ff2e94780667e127c049d3e2b8a15cf1
Steve Kelly <kd2cca@gmail.com>
2015-02-07 19:44:05 -0500

    some progress on basic types and operations

--- src/Bounds.jl
+++ src/Bounds.jl
@@ -1,27 +1,40 @@
 module Bounds
 
-abstract AbstractBounds{T, N}
+export AbstractBound, Bound, update!
 
-type Bounds{T, N<:Int} <: AbstractBounds{T, N}
+abstract AbstractBound{T, N}
+
+type Bound{T, N} <: AbstractBound{T, N}
     min::Vector{T}
     max::Vector{T}
 end
 
-function Bounds{T}(max::Vector{T}, min::Vector{T})
+function Bound{T}(max::Vector{T}, min::Vector{T})
     n = length(max)
     m = length(min)
     @assert n == m
-    Bounds{T, n}(max, min)
+    Bound{T, n}(min, max)
 end
 
-max(b::Bounds) = b.max
-min(b::Bounds) = b.min
+function Bound(t::DataType, n::Int)
+    max = fill(typemin(t), n)
+    min = fill(typemax(t), n)
+    Bound{t,n}(min, max)
+end
 
-function update!{T, N}(b::Bounds{T, N}, v)
-    for i = 1:n
+@inline Base.max(b::Bound) = b.max
+@inline Base.min(b::Bound) = b.min
+
+function update!{T, N}(b::Bound{T, N}, v)
+    for i = 1:N
         b.max[i] = max(b.max[i], v[i])
         b.min[i] = min(b.min[i], v[i])
     end
+end
+
+function (==){T1, N1, T2, N2}(b1::Bound{T1, N1}, b2::Bound{T2, N2})
+    N1 == N2 && b1.min == b2.min && b1.max == b2.max || return false
+    return true
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,15 @@ using Bounds
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+a = Bound(Float64, 4)
+@test a == Bound{Float64,4}([Inf,Inf,Inf,Inf],[-Inf,-Inf,-Inf,-Inf])
+
+update!(a, [1,2,3,4])
+
+@test a == Bound{Float64,4}([1.0,2.0,3.0,4.0],[1.0,2.0,3.0,4.0])
+@test a != Bound{Float64,4}([3.0,2.0,3.0,4.0],[1.0,2.0,3.0,4.0])
+
+update!(a, [5,6,7,8])
+
+@test a == Bound{Float64,4}([1.0,2.0,3.0,4.0],[5.0,6.0,7.0,8.0])
+
