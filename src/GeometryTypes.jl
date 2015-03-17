0f0bb4c73dab8ae0b848cf81b108e7c1a057773c
Simon Danisch <sdanisch@gmail.com>
2015-03-17 21:17:06 +0100

    Made matrices work

--- src/GeometryTypes.jl
+++ src/GeometryTypes.jl
@@ -1,6 +1,85 @@
 module GeometryTypes
 
 using FixedSizeArrays
+
 include("types.jl")
 
+# Immutable Vector type
+export Vector2
+export Vector3
+export Vector4
+
+#Mutable Vector type
+export MVector2
+export MVector3
+export MVector4
+
+# Immutable Point type
+export Point2
+export Point3
+export Point4
+
+#Mutable Point type
+export MPoint2
+export MPoint3
+export MPoint4
+
+# Immutable Normal type
+export Normal2
+export Normal3
+export Normal4
+
+#Mutable Normal type
+export MNormal2
+export MNormal3
+export MNormal4
+
+#Texture Coordinate Types
+export UV
+export UVW
+
+export MUV
+export MUVW
+
+#Face Types
+export Face3
+export Face4
+export Face5
+export Face6
+export Face7
+export Face8
+
+export MFace3
+export MFace4
+export MFace5
+export MFace6
+export MFace7
+export MFace8
+
+typealias Triangle{T} Face3{T}
+export Triangle
+typealias Quad{T} Face3{T}
+export Quad
+
+# Some primitives
+export Cube
+export Circle
+export Sphere
+export Rectangle
+
+export MCube
+export MCircle
+export MSphere
+export MRectangle
+export Matrix3x3
+
 end # module
+
+using GeometryTypes
+
+a =  Matrix3x3(
+	1,2,3,
+	4,5,6,
+	7,8,9)
+
+@show a*a
--- src/types.jl
+++ src/types.jl
@@ -1,25 +1,50 @@
+@gen_fixed_sizea_array("Vector", 	[:x,:y,:z,:w],              2:4)
+@gen_fixed_sizea_array("Point",  	[:x,:y,:z,:w], 				2:4)
+@gen_fixed_sizea_array("Normal",  	[:x,:y,:z,:w], 				2:4)
+@gen_fixed_sizea_array("UV",		[:u,:v], 					2:2)
+@gen_fixed_sizea_array("UVW",  		[:u,:v,:w], 				3:3)
+@gen_fixed_sizea_array("Face",   	[:a,:b,:c,:d,:e,:f,:g,:h], 	3:8)
 
-gen_fixed_sizea_array("Vector", 	[:x,:y,:z,:w],              1:4)
-gen_fixed_sizea_array("Point",  	[:x,:y,:z,:w], 				1:4)
-gen_fixed_sizea_array("Normal",  	[:x,:y,:z,:w], 				1:4)
-gen_fixed_sizea_array("UV",		    [:u,:v], 					2:2)
-gen_fixed_sizea_array("UVW",  		[:u,:v,:w], 				3:3)
-gen_fixed_sizea_array("Face",   	[:a,:b,:c,:d,:e,:f,:g,:h], 	3:8)
 
 
-#Axis Aligned Bounding Box
-immutable AABB{T}
+
+#Axis Aligned Cube
+immutable Cube{T}
   min::Vector3{T}
   max::Vector3{T}
 end
 
-immutable Circle{T <: Real}
-    x::T
-    y::T
+immutable Circle{T}
+    center::Point2{T}
     r::T
 end
+immutable Sphere{T}
+    center::Point3{T}
+    r::T
+end
+immutable Rectangle{T}
+    x::T
+    y::T
+    w::T
+    h::T
+end
 
-type Rectangle{T <: Real}
+
+
+type MCube{T}
+  min::MVector3{T}
+  max::MVector3{T}
+end
+
+type MCircle{T}
+    center::MPoint2{T}
+    r::T
+end
+type MSphere{T}
+    center::MPoint3{T}
+    r::T
+end
+type MRectangle{T}
     x::T
     y::T
     w::T
