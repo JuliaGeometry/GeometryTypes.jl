8c8af0464869a6996755eb4b6d030704962c3707
Sdanisch <sdanisch@gmail.com>
2015-08-25 00:30:56 +0200

    added convert

--- src/faces.jl
+++ src/faces.jl
@@ -12,14 +12,19 @@ function convert{FT1, FT2}(::Type{Vector{Face{3, FT1}}}, f::Vector{Face{4, FT2}}
 	return fsn
 end
 # This needs to be defined here, but should ultimately done by fixedsizearrays. Right now it would lead to ambiguities though.
-call{T, Offset, S <: AbstractString}(::Type{Face{3, T, Offset}}, x::Union(Vector{S}, Tuple{S})) = Face3{T, Offset}(parse(T, x[1]), parse(T, x[2]), parse(T, x[3]))
-call{T, Offset, S <: AbstractString}(::Type{Face{4, T, Offset}}, x::Union(Vector{S}, Tuple{S})) = Face4{T, Offset}(parse(T, x[1]), parse(T, x[2]), parse(T, x[3]), parse(T, x[4]))
+#call{T, Offset, S <: AbstractString}(::Type{Face{3, T, Offset}}, x::Union(Vector{S}, Tuple{S})) = Face3{T, Offset}(parse(T, x[1]), parse(T, x[2]), parse(T, x[3]))
+#call{T, Offset, S <: AbstractString}(::Type{Face{4, T, Offset}}, x::Union(Vector{S}, Tuple{S})) = Face4{T, Offset}(parse(T, x[1]), parse(T, x[2]), parse(T, x[3]), parse(T, x[4]))
 
 getindex{T,N,FD,FT,Offset}(a::Array{T,N}, i::Face{FD, FT, Offset})                 = a[[(i-Offset)...]]
 setindex!{T,N,FD,FT,Offset}(a::Array{T,N}, b::Array{T,N}, i::Face{FD, FT, Offset}) = (a[[(i-Offset)...]] = b)
 
-call{T, IndexOffset1}(::Type{Face{3, T, IndexOffset1}}, f::Face{3, T, IndexOffset1}) = f
-call{T1, T2, IndexOffset1, IndexOffset2}(t::Type{Face{3, T1, IndexOffset1}}, f::Face{3, T2, IndexOffset2}) = t((f+IndexOffset1-IndexOffset2)...)
+convert{T, IndexOffset1, N}(::Type{Face{N, T, IndexOffset1}}, f::Face{N, T, IndexOffset1}) = f
+convert{T1, T2, IndexOffset1, IndexOffset2, N}(t::Type{Face{N, T1, IndexOffset1}}, f::Face{N, T2, IndexOffset2}) = t((f+IndexOffset1-IndexOffset2)...)
+
+# Silly duplication, but call(::FixedVector, ::Any) = convert is overloaded in FixedSizeArrays
+call{T, IndexOffset1, N}(::Type{Face{N, T, IndexOffset1}}, f::Face{N, T, IndexOffset1}) = f
+call{T1, T2, IndexOffset1, IndexOffset2, N}(t::Type{Face{N, T1, IndexOffset1}}, f::Face{N, T2, IndexOffset2}) = t((f+IndexOffset1-IndexOffset2)...)
+
 
 getindex{T}(a::Array{T,2}, rect::Rectangle)                 = a[rect.x+1:rect.w, rect.y+1:rect.h]
 setindex!{T}(a::Array{T,2}, b::Array{T,2}, rect::Rectangle) = (a[rect.x+1:rect.w, rect.y+1:rect.h] = b)
