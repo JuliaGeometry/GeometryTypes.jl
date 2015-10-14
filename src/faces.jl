a64be4b5fa3ee8bc3d52a51891c9232f10af92d2
Steve Kelly <kd2cca@gmail.com>
2015-10-14 19:08:41 -0400

    tabs -> 4 spaces

--- src/faces.jl
+++ src/faces.jl
@@ -1,26 +1,30 @@
 # triangulate a quad. Could be written more generic
 export triangulate
 triangulate{FT1, FT2, Offset1, Offset2}(::Type{Face{3, FT1, Offset1}}, f::Face{4, FT2, Offset2}) =
-	(Face{3, FT1, Offset1}(Face{3, FT2, Offset2}(f[1:3])), Face{3, FT1, Offset1}(Face{3, FT2, Offset2}(f[(3, 4, 1)])))
+    (Face{3, FT1, Offset1}(Face{3, FT2, Offset2}(f[1:3])), Face{3, FT1, Offset1}(Face{3, FT2, Offset2}(f[(3, 4, 1)])))
 
 #=
 # need to spend more time on this generic conversion algorithm
 function convert{FT1, FT2, N, Offset1, Offset2}(::Type{Vector{Face{3, FT1, Offset1}}}, f::Vector{Face{N, FT2, Offset2}})
-	fsn = fill(Face{3, FT1, Offset1}, N-2)
-	for i=1:2:length(fsn)
-		a, b 	 = triangulate(Face{3, FT1, Offset1}, fs[div(i,2)])
-		fsn[i] 	 = a
-		fsn[i+1] = b
-	end
-	return fsn
+    fsn = fill(Face{3, FT1, Offset1}, N-2)
+    for i=1:2:length(fsn)
+        a, b 	 = triangulate(Face{3, FT1, Offset1}, fs[div(i,2)])
+        fsn[i] 	 = a
+        fsn[i+1] = b
+    end
+    return fsn
 end
 =#
 # This needs to be defined here, but should ultimately done by fixedsizearrays. Right now it would lead to ambiguities though.
 #call{T, Offset, S <: AbstractString}(::Type{Face{3, T, Offset}}, x::Union{Vector{S}, Tuple{S}}) = Face3{T, Offset}(parse(T, x[1]), parse(T, x[2]), parse(T, x[3]))
 #call{T, Offset, S <: AbstractString}(::Type{Face{4, T, Offset}}, x::Union{Vector{S}, Tuple{S}}) = Face4{T, Offset}(parse(T, x[1]), parse(T, x[2]), parse(T, x[3]), parse(T, x[4]))
 
-getindex{T,N,FD,FT,Offset}(a::Array{T,N}, i::Face{FD, FT, Offset})                 = a[[(map(Int,i)-Offset)...]]
-setindex!{T,N,FD,FT,Offset}(a::Array{T,N}, b::Array{T,N}, i::Face{FD, FT, Offset}) = (a[[(map(Int,i)-Offset)...]] = b)
+function getindex{T,N,FD,FT,Offset}(a::Array{T,N}, i::Face{FD, FT, Offset})
+    a[[(map(Int,i)-Offset)...]]
+end
+function setindex!{T,N,FD,FT,Offset}(a::Array{T,N}, b::Array{T,N}, i::Face{FD, FT, Offset})
+    a[[(map(Int,i)-Offset)...]] = b
+end
 
 convert{T, IndexOffset1, N}(::Type{Face{N, T, IndexOffset1}}, f::Face{N, T, IndexOffset1}) = f
 convert{T, T2, IndexOffset1, N}(::Type{Face{N, T, IndexOffset1}}, f::Face{N, T2, IndexOffset1}) = Face{N, T, IndexOffset1}(convert(NTuple{N, T}, f.(1)))
--- test/meshtypes.jl
+++ test/meshtypes.jl
@@ -1,29 +1,29 @@
 context("Mesh Types") do
 
 context("Merging Mesh") do
-	baselen = 0.4f0
-	dirlen  = 2f0
-	a    = [
-		(Cube{Float32}(Vec3f0(baselen), Vec3f0(dirlen, baselen, baselen)), RGBA(1f0,0f0,0f0,1f0)),
-		(Cube{Float32}(Vec3f0(baselen), Vec3f0(baselen, dirlen, baselen)), RGBA(0f0,1f0,0f0,1f0)),
-		(Cube{Float32}(Vec3f0(baselen), Vec3f0(baselen, baselen, dirlen)), RGBA(0f0,0f0,1f0,1f0))
-	]
-	am = map(GLNormalMesh, a)
-	axis = merge(am)
-	axis2 = merge(am)
-	@fact axis --> axis2 # test ==
-	@fact typeof(am[1]) --> GLNormalColorMesh
+    baselen = 0.4f0
+    dirlen = 2f0
+    a = [
+        (Cube{Float32}(Vec3f0(baselen), Vec3f0(dirlen, baselen, baselen)), RGBA(1f0,0f0,0f0,1f0)),
+        (Cube{Float32}(Vec3f0(baselen), Vec3f0(baselen, dirlen, baselen)), RGBA(0f0,1f0,0f0,1f0)),
+        (Cube{Float32}(Vec3f0(baselen), Vec3f0(baselen, baselen, dirlen)), RGBA(0f0,0f0,1f0,1f0))
+    ]
+    am = map(GLNormalMesh, a)
+    axis = merge(am)
+    axis2 = merge(am)
+    @fact axis --> axis2 # test ==
+    @fact typeof(am[1]) --> GLNormalColorMesh
 
-	@fact typeof(axis) --> GLNormalAttributeMesh
+    @fact typeof(axis) --> GLNormalAttributeMesh
 
-	@fact vertextype(axis) --> Point{3, Float32}
-	@fact normaltype(axis) --> Normal{3, Float32}
-	@fact facetype(axis) --> Face{3, Cuint, -1}
-	@fact hasvertices(axis) --> true
-	@fact hasfaces(axis) --> true
-	@fact hasnormals(axis) --> true
-	@fact hascolors(axis) --> false
-	@fact triangulate(GLTriangle, Face{4, Int, 0}(1,2,3,4)) --> (Face{3,UInt32,-1}((0x00000000,0x00000001,0x00000002)), Face{3,UInt32,-1}((0x00000002,0x00000003,0x00000000)))
+    @fact vertextype(axis) --> Point{3, Float32}
+    @fact normaltype(axis) --> Normal{3, Float32}
+    @fact facetype(axis) --> Face{3, Cuint, -1}
+    @fact hasvertices(axis) --> true
+    @fact hasfaces(axis) --> true
+    @fact hasnormals(axis) --> true
+    @fact hascolors(axis) --> false
+    @fact triangulate(GLTriangle, Face{4, Int, 0}(1,2,3,4)) --> (Face{3,UInt32,-1}((0x00000000,0x00000001,0x00000002)), Face{3,UInt32,-1}((0x00000002,0x00000003,0x00000000)))
 end
 
 
