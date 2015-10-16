807ffa38f8a06b46371c0410ebf6397f89fbcb15
Steve Kelly <kd2cca@gmail.com>
2015-10-15 21:13:48 -0400

    im just moving stuff around

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,7 +4,7 @@ typealias Vec3f0 Vec{3, Float32}
 
 
 facts("GeometryTypes") do
-    include("test_hyperrectangles.jl")
+    include("hyperrectangles.jl")
     include("meshtypes.jl")
     include("distancefields.jl")
     include("primitives.jl")
