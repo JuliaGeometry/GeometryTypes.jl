5a305c947cb8627c84c3a68095850cf94af72e8b
Steve Kelly <kd2cca@gmail.com>
2015-11-12 21:25:09 -0500

    move more methods to the decompose function. Closes #20

--- src/decompose.jl
+++ src/decompose.jl
@@ -24,6 +24,54 @@ Extract all line segments in a Face.
                                         f[$(i+1)]+$(-O2+O1))) for i = 1:N-1])
     # connect vertices N and 1
     push!(v.args, :(Face{2,$FT1,$O1}(f[$(N)]+$(-O2+O1),
-                                     f[$(1)]+$(-O2+O1))))
+                                     f[$(1)]+$(-O2+O1)))) # not enough dollars
     v
+end
+
+function decompose{T}(::Type{Point{3,T}}, rect::HyperRectangle{3, T})
+       (Point{3, T}(rect.minimum[1],rect.minimum[2],rect.minimum[3]),
+        Point{3, T}(rect.minimum[1],rect.minimum[2],rect.maximum[3]),
+        Point{3, T}(rect.minimum[1],rect.maximum[2],rect.minimum[3]),
+        Point{3, T}(rect.minimum[1],rect.maximum[2],rect.maximum[3]),
+        Point{3, T}(rect.maximum[1],rect.minimum[2],rect.minimum[3]),
+        Point{3, T}(rect.maximum[1],rect.minimum[2],rect.maximum[3]),
+        Point{3, T}(rect.maximum[1],rect.maximum[2],rect.minimum[3]),
+        Point{3, T}(rect.maximum[1],rect.maximum[2],rect.maximum[3]))
+end
+
+function decompose{T}(::Type{Point{2,T}}, rect::HyperRectangle{2, T})
+       (Point{2,T}(rect.minimum[1], rect.minimum[2]),
+        Point{2, T}(rect.minimum[1], rect.maximum[2]),
+        Point{2, T}(rect.maximum[1], rect.minimum[2]),
+        Point{2,T}(rect.maximum[1], rect.maximum[2]))
+end
+
+function decompose{PT}(T::Type{Point{2, PT}},r::Rectangle)
+   T[T(r.x, r.y),
+    T(r.x, r.y + r.h),
+    T(r.x + r.w, r.y + r.h),
+    T(r.x + r.w, r.y)]
+end
+
+function decompose{PT}(T::Type{Point{3, PT}},p::Pyramid)
+    leftup   = T(-p.width , p.width, PT(0)) / 2f0
+    leftdown = T(-p.width, -p.width, PT(0)) / 2f0
+    tip = T(p.middle + T(PT(0),PT(0),p.length))
+    lu  = T(p.middle + leftup)
+    ld  = T(p.middle + leftdown)
+    ru  = T(p.middle - leftdown)
+    rd  = T(p.middle - leftup)
+    T[tip, rd, ru,
+        tip, ru, lu,
+        tip, lu, ld,
+        tip, ld, rd,
+        rd,  ru, lu,
+        lu,  ld, rd]
+end
+
+function decompose{ET}(T::Type{Point{3, ET}}, q::Quad)
+   T[q.downleft,
+    q.downleft + q.height,
+    q.downleft + q.width + q.height,
+    q.downleft + q.width]
 end
--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,1 +1,14 @@
-Base.@deprecate triangulate{F<:Face}(t::Type{F}, f::Face) decompose(t, f)
+import Base.@deprecate
+
+@deprecate triangulate{F<:Face}(t::Type{F}, f::Face) decompose(t, f)
+
+@deprecate call{T}(t::Type{Vector{Point{3,T}}}, r::HyperRectangle{3, T}) decompose(t, r)
+@deprecate call{T}(t::Type{Vector{Point{2,T}}}, r::HyperRectangle{2, T}) decompose(t, r)
+
+
+# TODO
+# These are very tightly coupled to HomogenousMesh.
+getindex{PT}(r::Rectangle, t::Type{Point{2, PT}}) = decompose(t, r)
+getindex{PT}(p::Pyramid, t::Type{Point{3, PT}}) = decompose(t, p)
+getindex{ET}(q::Quad, t::Type{Point{3, ET}}) = decompose(t, q)
+
