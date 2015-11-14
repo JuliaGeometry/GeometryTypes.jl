a40d936b5f36575d6dfa7de43fbf099a7a880758
Steve Kelly <kd2cca@gmail.com>
2015-11-14 16:07:06 -0500

    wip [ci skip]

--- src/push.jl
+++ src/push.jl
@@ -3,7 +3,7 @@ Add a Simplex to a PureSimplicialComplex.
 """
 function Base.push!{N1,N2}(p::PureSimplicialComplex{N1}, s::Simplex{N2})
     @assert N2 <= N1 # otherwise we would have type unstability
-    for i = 1:N1
+    for i = 1:N2
         faces = decompose(Simplex{i}, s)
         for face in faces
             push!(p.simplices[i], face)
