1d1b69696de2ae9fbb21e5e8b5b77bab4ea3486f
Steve Kelly <kd2cca@gmail.com>
2015-12-01 21:35:48 -0500

    make getindex(::Array, ::Face) return a tuple since it is of deterministic length. Also provide unsafe version for `inbounds`ing

--- src/faces.jl
+++ src/faces.jl
@@ -1,6 +1,25 @@
-function getindex{T,N,FD,FT,Offset}(a::Array{T,N}, i::Face{FD, FT, Offset})
-    a[[(map(Int,i)-Offset)...]]
+"""
+Given an Array, `a`, and a face, `f`, return a tuple of numbers
+interpreting the values and offset of the face as indices into `A`.
+"""
+@generated function getindex{T,N,FD,FT,Offset}(a::Array{T,N},
+                                               f::Face{FD, FT, Offset})
+    v = Expr(:tuple)
+    for i = 1:FD
+        push!(v.args, Expr(:ref, :a, :(f[$i]-Offset)))
+    end
+    Expr(:(::), v, :(NTuple{FD,T}))
 end
+
+@generated function Base.unsafe_getindex{T,N,FD,FT,Offset}(a::Array{T,N},
+                                               f::Face{FD, FT, Offset})
+    v = Expr(:tuple)
+    for i = 1:FD
+        push!(v.args, Expr(:call, Base.unsafe_getindex, :a, :(f[$i]-Offset)))
+    end
+    Expr(:(::), v, :(NTuple{FD,T}))
+end
+
 function setindex!{T,N,FD,FT,Offset}(a::Array{T,N}, b::Array{T,N}, i::Face{FD, FT, Offset})
     a[[(map(Int,i)-Offset)...]] = b
 end
