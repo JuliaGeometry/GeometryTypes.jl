30256b713645df157f0eef49a85107a9d5917377
Steve Kelly <kd2cca@gmail.com>
2015-12-12 16:36:42 -0500

    misc. tests for SimpleRectangle

--- test/simplerectangle.jl
+++ test/simplerectangle.jl
@@ -1,7 +1,16 @@
 context("SimpleRectangle") do
     s = SimpleRectangle(1,2,3,4)
     @fact typeof(s) --> SimpleRectangle{Int}
+    @fact width(s) --> 3
+    @fact height(s) --> 4
+    @fact area(s) --> 12
+    @fact minimum(s) --> Point(1,2)
+    @fact maximum(s) --> Point(4,6)
     h = HyperRectangle(s)
     @fact typeof(h) --> HyperRectangle{2,Int}
     @fact h --> HyperRectangle(1,2,4,6)
+    h = HyperRectangle{3,Int}(s)
+    @fact h --> HyperRectangle(1,2,0,4,6,0)
+
+    @fact SimpleRectangle(Vec(2,3)) --> SimpleRectangle(0,0,2,3)
 end
