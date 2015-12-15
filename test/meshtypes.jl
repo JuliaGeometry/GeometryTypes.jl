5cd18c18b6b6e5cf1f98ddd3d7ae1b751cccd310
Steve Kelly <kd2cca@gmail.com>
2015-12-15 18:55:32 -0500

    improve slice test coverage

--- test/meshtypes.jl
+++ test/meshtypes.jl
@@ -46,7 +46,8 @@ end
 
 
 context("Slice") do
-    test_mesh = HomogenousMesh(Point{3,Float64}[Point{3,Float64}(0.0,0.0,10.0),
+    test_mesh = HomogenousMesh(Point{3,Float64}[
+     Point{3,Float64}(0.0,0.0,10.0),
      Point{3,Float64}(0.0,10.0,10.0),
      Point{3,Float64}(0.0,0.0,0.0),
      Point{3,Float64}(0.0,10.0,0.0),
@@ -58,21 +59,23 @@ context("Slice") do
      Face{3,Int,0}(3,7,5)
      Face{3,Int,0}(1,3,5)
      Face{3,Int,0}(1,2,3)
-     Face{3,Int,0}(3,2,4)
+     Face{3,Int,0}(2,4,3)
      Face{3,Int,0}(1,5,6)
      Face{3,Int,0}(2,1,6)
      Face{3,Int,0}(4,8,3)
      Face{3,Int,0}(3,8,7)
      Face{3,Int,0}(7,8,6)
-     Face{3,Int,0}(5,7,6)
+     Face{3,Int,0}(7,6,5)
      Face{3,Int,0}(2,6,4)
      Face{3,Int,0}(4,6,8)]
     )
     s1 = slice(test_mesh, 1.0)
     s2 = slice(test_mesh, 2.0)
+    s3 = slice(test_mesh, 0.0)
 
     @fact length(s1) --> 8
     @fact length(s1) --> length(s2)
+    @fact length(s3) --> 0
     exp1 = [([0.0,1.0],[0.0,0.0]),([0.0,0.0],[1.0,0.0]),
                       ([1.0,0.0],[10.0,0.0]),([10.0,0.0],[10.0,1.0]),
                       ([10.0,1.0],[10.0,10.0]),([10.0,10.0],[1.0,10.0]),
