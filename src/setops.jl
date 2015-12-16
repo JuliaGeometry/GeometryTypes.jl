06ed4531575f899efb75608ccdd8d312dbcbe710
Steve Kelly <kd2cca@gmail.com>
2015-12-15 19:11:33 -0500

    improve test coverage on SimpleRectangle setops and simplify intersect method

--- src/setops.jl
+++ src/setops.jl
@@ -16,18 +16,10 @@ Perform a intersection between two HyperRectangles.
 intersect{T,N}(h1::HyperRectangle{N, T}, h2::HyperRectangle{N, T}) =
     HyperRectangle{N, T}(max(minimum(h1), minimum(h2)),  min(maximum(h1), maximum(h2)))
 
-function intersect{T}(a::SimpleRectangle{T}, b::SimpleRectangle{T})
-    axrange = a.x:xwidth(a)
-    ayrange = a.y:yheight(a)
-
-    bxrange = b.x:xwidth(b)
-    byrange = b.y:yheight(b)
-
-    xintersect = intersect(axrange, bxrange)
-    yintersect = intersect(ayrange, byrange)
-    (isempty(xintersect) || isempty(yintersect) ) && return SimpleRectangle(zero(T), zero(T), zero(T), zero(T))
-    x,y   = first(xintersect), first(yintersect)
-    xw,yh = last(xintersect), last(yintersect)
-    SimpleRectangle(x,y, xw-x, yh-y)
+function intersect(a::SimpleRectangle, b::SimpleRectangle)
+    min_n = max(minimum(a), minimum(b))
+    max_n = min(maximum(a), maximum(b))
+    w = max_n - min_n
+    SimpleRectangle(min_n[1], min_n[2], w[1], w[2])
 end
 
--- test/simplerectangle.jl
+++ test/simplerectangle.jl
@@ -1,4 +1,5 @@
 context("SimpleRectangle") do
+context("constructors") do
     s = SimpleRectangle(1,2,3,4)
     @fact typeof(s) --> SimpleRectangle{Int}
     @fact width(s) --> 3
@@ -13,4 +14,15 @@ context("SimpleRectangle") do
     @fact h --> HyperRectangle(1,2,0,4,6,0)
 
     @fact SimpleRectangle(Vec(2,3)) --> SimpleRectangle(0,0,2,3)
+end
+
+context("setops") do
+    r1 = SimpleRectangle(0,0,2,2)
+    r2 = SimpleRectangle(2,2,2,2)
+    r3 = SimpleRectangle(1,1,2,2)
+    r4 = SimpleRectangle(0.5,0.5,3.,3.)
+    @fact intersect(r1,r2) --> SimpleRectangle(2,2,0,0)
+    @fact intersect(r1,r3) --> SimpleRectangle(1,1,1,1)
+    @fact intersect(r1,r4) --> SimpleRectangle(0.5,0.5,1.5,1.5)
+end
 end
