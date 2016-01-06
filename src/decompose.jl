ee2a4f4e42e9676898752403b482744084c12c79
SimonDanisch <sdanisch@gmail.com>
2016-01-06 19:02:28 +0100

    fix 80 chars

--- src/decompose.jl
+++ src/decompose.jl
@@ -57,7 +57,8 @@ Decompose an N-Simplex into tuple of Simplex{2}
 """
 @generated function decompose{N, T1, T2}(::Type{Simplex{2, T1}},
                                        f::Simplex{N, T2})
-    2 <= N || error("decompose not implented for N <= 2 yet. N: $N")# other wise degenerate
+    # other wise degenerate
+    2 <= N || error("decompose not implented for N <= 2 yet. N: $N")
 
     v = Expr(:tuple)
     append!(v.args, [:(Simplex{2,$T1}(f[$(i)],
@@ -69,7 +70,8 @@ Decompose an N-Simplex into tuple of Simplex{2}
 end
 
 # less strict version of above that preserves types
-decompose{N, T}(::Type{Simplex{2}}, f::Simplex{N, T}) = decompose(Simplex{2,T}, f)
+decompose{N, T}(::Type{Simplex{2}}, f::Simplex{N, T}) =
+    decompose(Simplex{2,T}, f)
 
 """
 Decompose an N-Simplex into a tuple of Simplex{1}
@@ -116,7 +118,7 @@ decompose{UVT}(T::Type{UV{UVT}}, r::SimpleRectangle) = T[
     T(1, 1),
     T(1, 0)
 ]
-decompose{FT, IndexOffset}(T::Type{Face{3, FT, IndexOffset}}, r::SimpleRectangle) = T[
+decompose{FT, IO}(T::Type{Face{3, FT, IO}}, r::SimpleRectangle) = T[
     Face{3, Int, 0}(1,2,3), Face{3, Int, 0}(3,4,1)
 ]
 
@@ -166,9 +168,8 @@ decompose{ET}(T::Type{UVW{ET}}, q::Quad) = T[
     q.downleft + q.width
 ]
 
-decompose{FT, IndexOffset}(T::Type{Face{3, FT, IndexOffset}}, r::Pyramid) = reinterpret(T, collect(map(FT,(1:18)+IndexOffset)))
-
-
+decompose{FT, IO}(T::Type{Face{3, FT, IO}}, r::Pyramid) =
+    reinterpret(T, collect(map(FT,(1:18)+IO)))
 
 # Getindex methods, for converted indexing into the mesh
 # Define decompose for your own meshtype, to easily convert it to Homogenous attributes
@@ -176,14 +177,14 @@ decompose{FT, IndexOffset}(T::Type{Face{3, FT, IndexOffset}}, r::Pyramid) = rein
 #Gets the normal attribute to a mesh
 function decompose{VT}(T::Type{Point{3, VT}}, mesh::AbstractMesh)
     vts = mesh.vertices
-    eltype(vts) == T       && return vts
-    eltype(vts) <: Point   && return map(T, vts)
+    eltype(vts) == T && return vts
+    eltype(vts) <: Point && return map(T, vts)
 end
 
 # gets the wanted face type
 function decompose{FT, Offset}(T::Type{Face{3, FT, Offset}}, mesh::AbstractMesh)
     fs = faces(mesh)
-    eltype(fs) == T       && return fs
+    eltype(fs) == T && return fs
     eltype(fs) <: Face{3} && return map(T, fs)
     if eltype(fs) <:  Face{4}
         convert(Vector{Face{3, FT, Offset}}, fs)
@@ -194,26 +195,26 @@ end
 #Gets the normal attribute to a mesh
 function decompose{NT}(T::Type{Normal{3, NT}}, mesh::AbstractMesh)
     n = mesh.normals
-    eltype(n) == T       && return n
+    eltype(n) == T && return n
     eltype(n) <: Normal{3} && return map(T, n)
-    n == Void[]       && return normals(mesh.vertices, mesh.faces, T)
+    n == Void[] && return normals(mesh.vertices, mesh.faces, T)
 end
 
 #Gets the uv attribute to a mesh, or creates it, or converts it
 function decompose{UVT}(::Type{UV{UVT}}, mesh::AbstractMesh)
     uv = mesh.texturecoordinates
-    eltype(uv) == UV{UVT}           && return uv
+    eltype(uv) == UV{UVT} && return uv
     (eltype(uv) <: UV || eltype(uv) <: UVW) && return map(UV{UVT}, uv)
-    eltype(uv) == Void           && return zeros(UV{UVT}, length(mesh.vertices))
+    eltype(uv) == Void && return zeros(UV{UVT}, length(mesh.vertices))
 end
 
 
 #Gets the uv attribute to a mesh
 function decompose{UVWT}(::Type{UVW{UVWT}}, mesh::AbstractMesh)
     uvw = mesh.texturecoordinates
-    typeof(uvw) == UVW{UVT}     && return uvw
-    (isa(uvw, UV) || isa(uv, UVW))  && return map(UVW{UVWT}, uvw)
-    uvw == nothing          && return zeros(UVW{UVWT}, length(mesh.vertices))
+    typeof(uvw) == UVW{UVT} && return uvw
+    (isa(uvw, UV) || isa(uv, UVW)) && return map(UVW{UVWT}, uvw)
+    uvw == nothing && return zeros(UVW{UVWT}, length(mesh.vertices))
 end
 const DefaultColor = RGBA(0.2, 0.2, 0.2, 1.0)
 
@@ -221,14 +222,14 @@ const DefaultColor = RGBA(0.2, 0.2, 0.2, 1.0)
 function decompose{T <: Color}(::Type{Vector{T}}, mesh::AbstractMesh)
     colors = mesh.attributes
     typeof(colors) == Vector{T} && return colors
-    colors == nothing           && return fill(DefaultColor, length(mesh.attribute_id))
+    colors == nothing && return fill(DefaultColor, length(mesh.attribute_id))
     map(T, colors)
 end
 
 #Gets the color attribute from a mesh
 function decompose{T <: Color}(::Type{T}, mesh::AbstractMesh)
     c = mesh.color
-    typeof(c) == T    && return c
-    c == nothing      && return DefaultColor
+    typeof(c) == T && return c
+    c == nothing && return DefaultColor
     convert(T, c)
 end
