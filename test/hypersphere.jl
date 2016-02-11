65d8163d520a1451eb0a852839c826254be28d25
SimonDanisch <sdanisch@gmail.com>
2016-02-11 21:36:21 +0100

    even more coverage

--- test/hypersphere.jl
+++ test/hypersphere.jl
@@ -16,4 +16,9 @@ context("HyperSphere") do
 
     centered_rect = centered(HyperSphere{3, Float32})
     @fact centered_rect --> HyperSphere{3,Float32}(Point3f0(0), 0.5f0)
+
+    @fact widths(centered_rect) --> Vec3f0(1)
+    @fact radius(centered_rect) --> 0.5f0
+    @fact maximum(centered_rect) --> Vec3f0(0.5f0)
+    @fact minimum(centered_rect) --> Vec3f0(-0.5f0)
 end
