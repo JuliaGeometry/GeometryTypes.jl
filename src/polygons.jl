60735fedc8943fa7da12786c00cea0c98ad21958
SimonDanisch <sdanisch@gmail.com>
2016-10-02 18:45:49 +0200

    link more actual implementation

--- src/polygons.jl
+++ src/polygons.jl
@@ -1,35 +1,7 @@
 #=
 ported from:
-COTD Entry submitted by John W. Ratcliff [jratcliff@verant.com]
-
-THIS IS A CODE SNIPPET WHICH WILL EFFICIEINTLY TRIANGULATE ANY
-POLYGON/CONTOUR (without holes) AS A STATIC CLASS.  THIS SNIPPET
-IS COMPRISED OF 3 FILES, TRIANGULATE.H, THE HEADER FILE FOR THE
-TRIANGULATE BASE CLASS, TRIANGULATE.CPP, THE IMPLEMENTATION OF
-THE TRIANGULATE BASE CLASS, AND TEST.CPP, A SMALL TEST PROGRAM
-DEMONSTRATING THE USAGE OF THE TRIANGULATOR.  THE TRIANGULATE
-BASE CLASS ALSO PROVIDES TWO USEFUL HELPER METHODS, ONE WHICH
-COMPUTES THE AREA OF A POLYGON, AND ANOTHER WHICH DOES AN EFFICENT
-POINT IN A TRIANGLE TEST.
-SUBMITTED BY JOHN W. RATCLIFF (jratcliff@verant.com) July 22, 2000
-
- **********************************************************************/
- ************ HEADER FILE FOR TRIANGULATE.H ***************************/
- **********************************************************************/
- *****************************************************************/
- ** Static class to triangulate any contour/polygon efficiently **/
- ** You should replace Vector2d with whatever your own Vector   **/
- ** class might be.  Does not support polygons with holes.      **/
- ** Uses STL vectors to represent a dynamic array of vertices.  **/
- ** This code snippet was submitted to FlipCode.com by          **/
- ** John W. Ratcliff (jratcliff@verant.com) on July 22, 2000    **/
- ** I did not write the original code/algorithm for this        **/
- ** this triangulator, in fact, I can't even remember where I   **/
- ** found it in the first place.  However, I did rework it into **/
- ** the following black-box static class so you can make easy   **/
- ** use of it in your own code.  Simply replace Vector2d with   **/
- ** whatever your own Vector implementation might be.           **/
- *****************************************************************/
+http://docs.ros.org/jade/api/convex_decomposition/html/triangulate_8cpp_source.html
+Author of C version: John W. Ratcliff
 =#
 
 function area{N, T}(contour::AbstractVector{Point{N, T}})
